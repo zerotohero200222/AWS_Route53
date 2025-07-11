96e48ae74203b348f06a4d1377131147db15afdf
zerotohero200222 <aebharath17@gmail.com>
2025-07-11 16:24:40 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
-  required_version = ">= 1.3.0"
-}
-
 provider "aws" {
   region = var.region
 }
